552c9136a6aaeb6632a25535157632a6d918ae94
Aito Hideki <aito.hideki@gmail.com>
2022-05-06 05:27:23 -0400

    feat(flights): updated flight board layout

--- src/components/flight/board.vue
+++ src/components/flight/board.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container max-h-screen overflow-hidden mx-auto py-4">
+  <div class="container max-h-screen overflow-hidden mx-auto py-4 px-4">
     <main-alert class="mb-4" />
     <div class="max-w-full flex row">
       <flight-list class="mr-4" />
