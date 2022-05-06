c362fee78d8c9bd216c7abefd26db01ce8832fb4
Aito Hideki <aito.hideki@gmail.com>
2022-05-05 22:58:55 -0400

    ci(components): auto component importing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteESLint from '@ehutch79/vite-eslint'
+import components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     vue(),
     process.env.NODE_ENV === 'development' && viteESLint({
       include: ['src/**/*.vue', 'src/**/*.ts']
-    })
+    }),
+    components({ directoryAsNamespace: true }),
   ]
 })
