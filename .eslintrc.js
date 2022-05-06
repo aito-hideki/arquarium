5ae4ff725aa2d178bda967fa690bc73d421708fb
Aito Hideki <aito.hideki@gmail.com>
2022-05-06 05:26:18 -0400

    ci(lint): lint setting updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   rules: {
     camelcase: 'off',
+    'func-call-spacing': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/max-len': ['error', {
