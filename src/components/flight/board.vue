0b8d18327e1248b16f65c18997a53f41e24c4e70
Aito Hideki <aito.hideki@gmail.com>
2022-05-06 05:10:51 -0400

    feat(layout): updated app layout

---
+++ src/components/flight/board.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container max-h-screen overflow-hidden mx-auto py-4">
+    <main-alert class="mb-4" />
+    <div class="max-w-full flex row">
+      <flight-list class="mr-4" />
+      <flight-details />
+    </div>
+  </div>
+</template>
